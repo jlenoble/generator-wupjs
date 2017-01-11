5f70408cf591ddc926765f759fd67097ab2af279
Jason Lenoble <jason.lenoble@gmail.com>
2017-01-11 01:45:54 +0100

    Changed package main to file in lib dir

--- generators/app/templates/gulp/dist.js
+++ generators/app/templates/gulp/dist.js
@@ -1,17 +1,15 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';<% if (transpile !== 'none') { %>
 import babel from 'gulp-babel';<% } %>
-import rename from 'gulp-rename';
 
-import {srcGlob} from './globs';
+import {srcGlob, distDir} from './globs';
 
 export const dist = () => {
   return gulp.src(srcGlob)
     .pipe(eslint())
     .pipe(eslint.format())<% if (transpile !== 'none') { %>
     .pipe(babel())<% } %>
-    .pipe(rename('index.js'))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest(distDir));
 };
 
 gulp.task('dist', dist);
