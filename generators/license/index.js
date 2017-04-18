d9ca734cde6671319382ac979dd31e018600c0ea
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-18 21:32:07 +0200

    generator license

---
+++ generators/license/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _base = require('../base');
+
+var _base2 = _interopRequireDefault(_base);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _class = function (_Base) {
+  _inherits(_class, _Base);
+
+  function _class(args, opts) {
+    _classCallCheck(this, _class);
+
+    var options = Object.assign({
+      props: ['license'],
+      generator: 'license'
+    }, opts);
+
+    var _this = _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, args, options));
+
+    _this.composeWith('date');
+    return _this;
+  }
+
+  _createClass(_class, [{
+    key: 'prompting',
+    value: function prompting() {
+      var _this2 = this;
+
+      var prompts = [{
+        type: 'list',
+        name: 'license',
+        message: 'LICENSE:',
+        choices: ['MIT', 'GPL-3.0'],
+        default: this.get('license')
+      }];
+
+      return this.prompt(prompts).then(function (props) {
+        _this2.set(props);
+      });
+    }
+  }]);
+
+  return _class;
+}(_base2.default);
+
+exports.default = _class;
+module.exports = exports['default'];
---
+++ src/license/index.js
@@ -0,0 +1,28 @@
+import Base from '../base';
+
+export default class extends Base {
+  constructor (args, opts) {
+    const options = Object.assign({
+      props: ['license'],
+      generator: 'license',
+    }, opts);
+
+    super(args, options);
+
+    this.composeWith('date');
+  }
+
+  prompting () {
+    const prompts = [{
+      type: 'list',
+      name: 'license',
+      message: 'LICENSE:',
+      choices: ['MIT', 'GPL-3.0'],
+      default: this.get('license'),
+    }];
+
+    return this.prompt(prompts).then(props => {
+      this.set(props);
+    });
+  }
+}
