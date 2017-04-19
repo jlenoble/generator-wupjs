5a0801abd2982f490edd7bb87a5046a802d6c3c9
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-19 17:58:43 +0200

    generator: 'write-license' as option

--- src/write-license/index.js
+++ src/write-license/index.js
@@ -2,7 +2,11 @@ import Base from '../base';
 
 export default class extends Base {
   constructor (args, opts) {
-    super(args, opts);
+    const options = Object.assign({
+      generator: 'write-license',
+    }, opts);
+
+    super(args, options);
 
     this.promptIfMissing(['author', 'email', 'license', 'created', 'updated']);
   }
