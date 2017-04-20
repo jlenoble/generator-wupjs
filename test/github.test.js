019c0421b82e9228b15f5614da64905d58c290b7
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-20 17:15:31 +0200

    Test for package.json file

--- test/github.test.js
+++ test/github.test.js
@@ -1,3 +1,8 @@
 import {testGenerator} from './helpers';
 
-testGenerator('github', {github: 'me'}, [/"github": "me"/]);
+testGenerator('github', {github: 'me'}, {
+  '.yo-rc.json': [/"github": "me"/],
+  'package.json': [
+    /"repository": \{\s*"type": "git",\s*"url": "git:\/\/github\.com\/me\/[0-9a-f]{40}\.git"\s*\}/,
+  ],
+});
