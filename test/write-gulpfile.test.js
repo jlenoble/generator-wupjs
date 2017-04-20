3b31480ca3349847a2b469711bd7f8c5fbda0aeb
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-20 18:05:57 +0200

    Testing generator write-gulpfile

---
+++ test/write-gulpfile.test.js
@@ -0,0 +1,7 @@
+import {testGenerator} from './helpers';
+
+testGenerator('write-gulpfile', undefined, {
+  '.yo-rc.json': [/"devDeps": \{\s*"gulp": "git:\/\/github\.com\/gulpjs\/gulp\.git#4\.0"\s*\}/],
+  'package.json': [/"devDependencies": \{\s*"gulp":\s*"git:\/\/github\.com\/gulpjs\/gulp\.git#4\.0"\s*\}/],
+  'gulpfile.js': [],
+});
