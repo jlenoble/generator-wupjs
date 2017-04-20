d4fed308563a90692435a5406fa9c5322f7e636c
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-20 17:09:36 +0200

    Test for LICENSE file

--- test/date.test.js
+++ test/date.test.js
@@ -1,3 +1,7 @@
 import {testGenerator} from './helpers';
 
-testGenerator('date');
+testGenerator('date', undefined, {
+  'LICENSE': [
+    /Copyright \(c\) \d{4}(-\d{4})? \w+( \w+)* <(\w|\.|-|_|\d)+@(\w|\.|-|_|\d)+>/,
+  ],
+});
