3eee68907b74813646075a9c3038aa8a9b6b0277
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-20 17:02:54 +0200

    Test LICENSE and package.json files

--- test/who.test.js
+++ test/who.test.js
@@ -3,7 +3,15 @@ import {testGenerator} from './helpers';
 testGenerator('who', {
   author: 'Me Me',
   email: 'me@there',
-}, [
-  /"author": "Me Me"/,
-  /"email": "me@there"/,
-]);
+}, {
+  '.yo-rc.json': [
+    /"author": "Me Me"/,
+    /"email": "me@there"/,
+  ],
+  'LICENSE': [
+    /Copyright \(c\) \d{4}(-\d{4})? Me Me <me@there>/,
+  ],
+  'package.json': [
+    /"author": \{\s*"name": "Me Me",\s*"email": "me@there"\s*\}/,
+  ],
+});
