ec8d9bbf4dffcdd333a49b6fb35ee006ed741d2c
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-20 17:25:46 +0200

    Testing file package.json

--- test/paths.test.js
+++ test/paths.test.js
@@ -5,11 +5,16 @@ testGenerator('paths', {
   srcDir: 'src',
   testDir: 'test',
   buildDir: 'build',
-  libDir: 'lib',
-}, [
-  /"gulpDir": "gulp"/,
-  /"srcDir": "src"/,
-  /"testDir": "test"/,
-  /"buildDir": "build"/,
-  /"libDir": "lib"/,
-]);
+  libDir: 'deploy',
+}, {
+  '.yo-rc.json': [
+    /"gulpDir": "gulp"/,
+    /"srcDir": "src"/,
+    /"testDir": "test"/,
+    /"buildDir": "build"/,
+    /"libDir": "deploy"/,
+  ],
+  'package.json': [
+    /"main": "deploy\/[0-9a-f]{40}\.js"/,
+  ],
+});
