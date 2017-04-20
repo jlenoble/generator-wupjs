833daaf0570aeefc1acdddfedb4b097f73b1a15d
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-20 17:22:43 +0200

    Testing file package.json

--- test/module.test.js
+++ test/module.test.js
@@ -3,7 +3,13 @@ import {testGenerator} from './helpers';
 testGenerator('module', {
   name: 'my-module',
   description: 'Some description',
-}, [
-  /"name": "my-module"/,
-  /"description": "Some description"/,
-]);
+}, {
+  '.yo-rc.json': [
+    /"name": "my-module"/,
+    /"description": "Some description"/,
+  ],
+  'package.json': [
+    /"name": "my-module"/,
+    /"description": "Some description"/,
+  ],
+});
