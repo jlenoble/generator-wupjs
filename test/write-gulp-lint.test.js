57afc3f75373749866ea9c3bbf67a4a6818149dc
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-22 11:29:06 +0200

    Works with React

--- test/write-gulp-lint.test.js
+++ test/write-gulp-lint.test.js
@@ -16,3 +16,24 @@ testGenerator('write-gulp-lint', undefined, {
     /import '\.\/gulp\/lint';/,
   ],
 });
+
+testGenerator('write-gulp-lint', {addons: ['React']}, {
+  'gulp/lint.js': [
+    /import eslint from 'gulp-eslint';/,
+    /const allSrcGlob = \[/,
+    /'src\/\*\*\/\*\.js'/,
+    /'src\/\*\*\/\*\.jsx'/,
+    /'test\/\*\*\/\*\.js'/,
+    /'test\/\*\*\/\*\.jsx'/,
+    /\.pipe\(eslint\(\)\)/,
+    /\.pipe\(eslint\.format\(\)\)/,
+  ],
+  'package.json': [
+    /"gulp-eslint": "\*"/,
+    /"react": "\*"/,
+    /"react-dom": "\*"/,
+  ],
+  'gulpfile.babel.js': [
+    /import '\.\/gulp\/lint';/,
+  ],
+});
