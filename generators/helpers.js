2212c939020988decd477528aefd5d73723fdf9f
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-29 00:38:56 +0200

    removed unused deps

--- generators/helpers.js
+++ generators/helpers.js
@@ -4,15 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _compute = require('./compute');
-
-Object.defineProperty(exports, 'deps', {
-  enumerable: true,
-  get: function get() {
-    return _compute.deps;
-  }
-});
-
 var _config = require('./config');
 
 Object.defineProperty(exports, 'Config', {
@@ -55,36 +46,12 @@ Object.defineProperty(exports, 'getWriteGenerators', {
   }
 });
 
-var _joinGlobs = require('./join-globs');
-
-Object.defineProperty(exports, 'joinGlobs', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_joinGlobs).default;
-  }
-});
-
 var _property = require('./property');
 
 Object.defineProperty(exports, 'property', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_property).default;
-  }
-});
-
-var _stems = require('./stems');
-
-Object.defineProperty(exports, 'dirs', {
-  enumerable: true,
-  get: function get() {
-    return _stems.dirs;
-  }
-});
-Object.defineProperty(exports, 'globs', {
-  enumerable: true,
-  get: function get() {
-    return _stems.globs;
   }
 });
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,5 @@
-export {deps} from './compute';
 export {default as Config} from './config';
 export {default as extendProps, extendedProps} from './extend-props';
 export {default as getGenerator} from './get-generator';
 export {default as getWriteGenerators} from './get-write-generators';
-export {default as joinGlobs} from './join-globs';
 export {default as property} from './property';
-export {dirs, globs} from './stems';
