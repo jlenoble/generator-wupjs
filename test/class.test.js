902af6b1643dfef0a4e325580c3caebbaa9ef05f
Jason Lenoble <jason.lenoble@gmail.com>
2017-04-30 13:21:19 +0200

    Tested gen class with argument
    Was only tested with prompt

--- test/class.test.js
+++ test/class.test.js
@@ -1,6 +1,21 @@
 import {testGenerator} from './helpers';
 
-testGenerator('class', {name: 'Compo'}, {
-  'src/compo.js': true,
-  'test/compo.test.js': true,
+testGenerator('class', {name: 'My Class'}, {
+  'src/my-class.js': true,
+  'test/my-class.test.js': true,
+});
+
+testGenerator('class', {name: 'My   Class'}, {
+  'src/my-class.js': true,
+  'test/my-class.test.js': true,
+});
+
+testGenerator('class', {name: 'MyClass'}, {
+  'src/myclass.js': true,
+  'test/myclass.test.js': true,
+});
+
+testGenerator('class MyClass', undefined, {
+  'src/my-class.js': true,
+  'test/my-class.test.js': true,
 });
--- test/helpers.js
+++ test/helpers.js
@@ -1,77 +1,83 @@
 import path from 'path';
 import assert from 'yeoman-assert';
 import helpers from 'yeoman-test';
+import parseArgs from 'minimist';
 import Base from '../../generators/base';
 
-export const testGenerator = (name, _prompt = {}, assertContent = []) =>
-describe(`generator-wupjs:${name}`, function () {
-  const prompt = Object.assign({
-    description: 'Dummy description',
-    addons: [],
-    babel: 'none',
-    linters: [],
-    preprocessors: [],
-  }, _prompt);
+export const testGenerator = (_name, _prompt = {}, assertContent = []) => {
+  const parsed = parseArgs(_name.split(/\s+/));
+  const [name, ...args] = parsed._;
 
-  before(function () {
-    this.cwd = process.cwd();
-    Base.reset();
-    this.runContext = helpers
-      .run(path.join(__dirname, `../../generators/${name}`))
-      .withPrompts(prompt)
-      .toPromise();
+  describe(`generator-wupjs:${name}`, function () {
+    const prompt = Object.assign({
+      description: 'Dummy description',
+      addons: [],
+      babel: 'none',
+      linters: [],
+      preprocessors: [],
+    }, _prompt);
 
-    return this.runContext;
-  });
+    before(function () {
+      this.cwd = process.cwd();
+      Base.reset();
+      this.runContext = helpers
+        .run(path.join(__dirname, `../../generators/${name}`))
+        .withArguments(args)
+        .withPrompts(prompt)
+        .toPromise();
 
-  after(function () {
-    process.chdir(this.cwd);
-  });
-
-  const tests = {
-    '.yo-rc.json': [
-      /"genVersion": "0.2.16"/,
-      /"created": "\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{3}Z"/,
-      /"updated": "\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{3}Z"/,
-    ],
-  };
-
-  if (Array.isArray(assertContent)) {
-    tests['.yo-rc.json'] = tests['.yo-rc.json'].concat(assertContent);
-  } else {
-    Object.keys(assertContent).forEach(file => {
-      tests[file] = Array.isArray(tests[file]) ?
-        tests[file].concat(Array.isArray(assertContent[file]) ?
-          assertContent[file] : []) : assertContent[file];
+      return this.runContext;
     });
-  }
 
-  Object.keys(tests).forEach(file => {
-    if (file[0] !== '!' && tests[file]) {
-      it(`creates a ${file} file`, function () {
-        assert.file(file);
-      });
+    after(function () {
+      process.chdir(this.cwd);
+    });
 
-      if (tests[file] !== true) {
-        tests[file].forEach(content => {
-          it(`${file} has the expected content ${content}`, function () {
-            assert.fileContent(file, content);
-          });
-        });
-      }
+    const tests = {
+      '.yo-rc.json': [
+        /"genVersion": "0.2.16"/,
+        /"created": "\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{3}Z"/,
+        /"updated": "\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{3}Z"/,
+      ],
+    };
+
+    if (Array.isArray(assertContent)) {
+      tests['.yo-rc.json'] = tests['.yo-rc.json'].concat(assertContent);
     } else {
-      if (tests[file]) {
-        tests[file].forEach(content => {
-          const _file = file.substring(1);
-          it(`${_file} has not the content ${content}`, function () {
-            assert.noFileContent(_file, content);
-          });
-        });
-      } else {
-        it(`${file} is missing as expected`, function () {
-          assert.noFile(file);
-        });
-      }
+      Object.keys(assertContent).forEach(file => {
+        tests[file] = Array.isArray(tests[file]) ?
+          tests[file].concat(Array.isArray(assertContent[file]) ?
+            assertContent[file] : []) : assertContent[file];
+      });
     }
+
+    Object.keys(tests).forEach(file => {
+      if (file[0] !== '!' && tests[file]) {
+        it(`creates a ${file} file`, function () {
+          assert.file(file);
+        });
+
+        if (tests[file] !== true) {
+          tests[file].forEach(content => {
+            it(`${file} has the expected content ${content}`, function () {
+              assert.fileContent(file, content);
+            });
+          });
+        }
+      } else {
+        if (tests[file]) {
+          tests[file].forEach(content => {
+            const _file = file.substring(1);
+            it(`${_file} has not the content ${content}`, function () {
+              assert.noFileContent(_file, content);
+            });
+          });
+        } else {
+          it(`${file} is missing as expected`, function () {
+            assert.noFile(file);
+          });
+        }
+      }
+    });
   });
-});
+};
