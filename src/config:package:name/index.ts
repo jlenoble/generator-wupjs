5552b8e74e62640b0260c9678727db2046535381
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-10 12:56:40 +0200

    prompting() calls _IsValid()
    Better separates concerns
    Also added prefix Package to class name

--- src/config:package:name/index.ts
+++ src/config:package:name/index.ts
@@ -1,6 +1,6 @@
 import Base from "../common/base-generator";
 
-export default class Name extends Base {
+export default class PackageName extends Base {
   public constructor(args: string | string[], options: {}) {
     super(
       args,
@@ -9,6 +9,29 @@ export default class Name extends Base {
         willWrite: ["write:package.json"]
       })
     );
+  }
+
+  protected _isValid(props: Wup.Options): boolean {
+    const name: Wup.GenName = props[this.generatorName] as Wup.GenName;
+
+    if (name.length > 214) {
+      console.warn(`The name must be less than or equal to 214 characters.
+This includes the scope for scoped packages.`);
+    } else if (name[0] === "." || name[0] === "_") {
+      console.warn("The name can’t start with a dot or an underscore.");
+    } else if (name.toLowerCase() !== name) {
+      console.warn("New packages must not have uppercase letters in the name.");
+    } else if (/\s/.test(name)) {
+      console.warn(`The name ends up being part of a URL, an argument on the command line, and a
+folder name. So don't use space characters`);
+    } else if (encodeURIComponent(name) !== name) {
+      console.warn(`The name ends up being part of a URL, an argument on the command line, and a
+folder name. Therefore, the name can’t contain any non-URL-safe characters.`);
+    } else {
+      return true;
+    }
+
+    return false;
   }
 
   public initializing(): void {
@@ -27,24 +50,8 @@ export default class Name extends Base {
       ];
 
       const props = await this.prompt(prompts);
-      const name = props[this.generatorName];
 
-      if (name.length > 214) {
-        console.warn(`The name must be less than or equal to 214 characters.
-This includes the scope for scoped packages.`);
-      } else if (name[0] === "." || name[0] === "_") {
-        console.warn("The name can’t start with a dot or an underscore.");
-      } else if (name.toLowerCase() !== name) {
-        console.warn(
-          "New packages must not have uppercase letters in the name."
-        );
-      } else if (/\s/.test(name)) {
-        console.warn(`The name ends up being part of a URL, an argument on the command line, and a
-folder name. So don't use space characters`);
-      } else if (encodeURIComponent(name) !== name) {
-        console.warn(`The name ends up being part of a URL, an argument on the command line, and a
-folder name. Therefore, the name can’t contain any non-URL-safe characters.`);
-      } else {
+      if (this._isValid(props)) {
         this.setProp(props);
         return;
       }
