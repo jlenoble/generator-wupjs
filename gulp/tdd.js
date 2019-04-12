ec597f0d5c6a5e761b0a966f7bf1eed4506b59a0
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-12 09:53:36 +0200

    watch ejs files

--- gulp/tdd.js
+++ gulp/tdd.js
@@ -2,7 +2,8 @@ import gulp from "gulp";
 
 // import "./test";
 import "./build";
+import "./copy";
 import "./watch";
 
 // gulp.task('tdd', gulp.series('test', 'watch'));
-gulp.task("tdd", gulp.series("build", "watch"));
+gulp.task("tdd", gulp.series("build", "copy", "watch"));
--- gulp/watch.js
+++ gulp/watch.js
@@ -1,13 +1,15 @@
 import gulp from "gulp";
 
-import { allSrcGlob, allBuildGlob, srcGlob } from "./globs";
+import { allSrcGlob, allBuildGlob, templateGlob, srcGlob } from "./globs";
 import { build, generate } from "./build";
+import { copy } from "./copy";
 import { test } from "./test";
 
 export const watch = done => {
   gulp.watch(allSrcGlob, build);
   gulp.watch(srcGlob, generate);
   // gulp.watch(allBuildGlob, test);
+  gulp.watch(templateGlob, copy);
   done();
 };
 
