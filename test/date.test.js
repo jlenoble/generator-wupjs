e7a32bfae9bcec62d7f21283cfa68f74261af04d
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-15 13:41:46 +0200

    Removed all redundant tests

--- test/date.test.js
+++
@@ -1,7 +0,0 @@
-import {testGenerator} from './helpers';
-
-testGenerator('date', undefined, {
-  'LICENSE': [
-    /Copyright \(c\) \d{4}(-\d{4})? \w+( \w+)* <(\w|\.|-|_|\d)+@(\w|\.|-|_|\d)+>/,
-  ],
-});
