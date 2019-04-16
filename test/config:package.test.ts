ebfc54e10d4419804d0366682547a88b412265db
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-16 09:42:38 +0200

    Tested subgen config:package

---
+++ test/config:package.test.ts
@@ -0,0 +1,14 @@
+import testGenerator from "./common/test-generator";
+
+testGenerator({
+  command: "yo wupjs:config:package",
+  prompt: {
+    "config:package:name": "awesome-app",
+    "config:package:version": "1.2.3",
+    "config:package:description": "Some dummy description",
+    "config:package:keywords": ["foo", "bar", "quux"]
+  },
+  assertContent: {
+    "package.json": true
+  }
+});
