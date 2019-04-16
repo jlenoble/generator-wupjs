c35e8568a8df5f1b6e7944b9ef2b78c1a784d3b9
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-16 09:41:50 +0200

    Tested subgen package:keywords

---
+++ test/config:package:keywords.test.ts
@@ -0,0 +1,11 @@
+import testGenerator from "./common/test-generator";
+
+testGenerator({
+  command: "yo wupjs:config:package:keywords",
+  prompt: {
+    "config:package:keywords": ["foo", "bar", "quux"]
+  },
+  assertContent: {
+    "package.json": true
+  }
+});
