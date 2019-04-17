69b7e266ee312e11b32ab1761189829bbe9352a0
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-17 12:05:57 +0200

    Snapshot for French author name

--- test/config:author:name.test.ts
+++ test/config:author:name.test.ts
@@ -8,3 +8,30 @@ testGenerator({
   },
   assertContent: {}
 });
+
+testGenerator({
+  title: "Setting an author name - French",
+  command: "yo wupjs:config:author:name",
+  prompt: {
+    "config:author:name": "François d'Esparbès de Lussan d'Aubeterre"
+  },
+  assertContent: {}
+});
+
+// testGenerator({
+//   title: "Setting an author name - Japanese",
+//   command: "yo wupjs:config:author:name",
+//   prompt: {
+//     "config:author:name": "ルネ・デカルト"
+//   },
+//   assertContent: {}
+// });
+//
+// testGenerator({
+//   title: "Setting an author name - Chinese",
+//   command: "yo wupjs:config:author:name",
+//   prompt: {
+//     "config:author:name": "艾薩克·牛頓"
+//   },
+//   assertContent: {}
+// });
