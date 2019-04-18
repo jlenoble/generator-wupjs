64c4c20188eb2455841a2fa6ddae1a46d33e6308
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-18 13:51:19 +0200

    Snapshot for default author name

--- test/config:author:name.test.ts
+++ test/config:author:name.test.ts
@@ -2,6 +2,11 @@ import testGenerator from "./common/test-generator";
 
 testGenerator({
   title: "Setting an author name",
+  command: "yo wupjs:config:author:name"
+});
+
+testGenerator({
+  title: "Setting an author name",
   command: "yo wupjs:config:author:name",
   prompt: {
     "config:author:name": "Me Me"
