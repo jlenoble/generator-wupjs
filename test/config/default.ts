237a2eaa79d6c04e8f39b414b9fe20f53474b947
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-18 19:20:18 +0200

    added github username to test config

--- test/config/default.ts
+++ test/config/default.ts
@@ -1,4 +1,5 @@
 export default {
   "config:author:name": "John Doe",
-  "config:author:email": "john.doe@example.com"
+  "config:author:email": "john.doe@example.com",
+  "config:github:username": "johndoe"
 };
