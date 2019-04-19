cba7994e8be5d704f248e94dad1f6258909523ad
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-19 15:43:56 +0200

    Snapshot for write:package.json with github:username

--- test/write:package.json.test.ts
+++ test/write:package.json.test.ts
@@ -14,3 +14,19 @@ testGenerator({
   },
   assertContent: {}
 });
+
+testGenerator({
+  title: "Prompting for package.json - setting github username",
+  command: "yo wupjs:write:package.json",
+  prompt: {
+    "config:author:name": "Me Me",
+    "config:author:email": "me@example.com",
+    "config:license": ["MIT"],
+    "config:package:name": "awesome-app",
+    "config:package:version": "1.2.3",
+    "config:package:description": "Some dummy description",
+    "config:package:keywords": "foo, bar, quux",
+    "config:github:username": "me-me"
+  },
+  assertContent: {}
+});
