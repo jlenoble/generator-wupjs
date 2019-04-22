8ee4e554740536dcc4b8076ebe0fe98766de08f2
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-22 16:53:19 +0200

    Snapshot for typescript setup

---
+++ snapshots/27bb1314f40a52d94735907927a75d8541ff173d/src/index.ts
@@ -0,0 +1,5 @@
+export default class AwesomeApp {
+  public constructor() {
+    console.log("Hello world!");
+  }
+}
--- test/app.test.ts
+++ test/app.test.ts
@@ -18,3 +18,23 @@ testGenerator({
     "test/index.test.js": true
   }
 });
+
+testGenerator({
+  title: "Testing main generator: with Typescript",
+  command: "yo wupjs",
+  prompt: {
+    "config:author:name": "Me Me",
+    "config:author:email": "me@example.com",
+    "config:license": ["MIT"],
+    "config:package:name": "awesome-app",
+    "config:package:version": "1.2.3",
+    "config:package:description": "Some dummy description",
+    "config:package:keywords": "foo, bar, quux",
+    "config:github:username": "me-me",
+    "config:languages:typescript": true
+  },
+  assertContent: {
+    "src/index.ts": true,
+    "test/index.test.ts": true
+  }
+});
