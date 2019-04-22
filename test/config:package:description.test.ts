52335f8f4e58b809329cfb23459a56f7aeb87b3c
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-22 14:45:59 +0200

    Removed snapshots and tests
    Will only test for now for the whole app
    (it's taking too much time)

--- test/config:package:description.test.ts
+++
@@ -1,9 +0,0 @@
-import testGenerator from "./common/test-generator";
-
-testGenerator({
-  title: "Setting a package description",
-  command: "yo wupjs:config:package:description",
-  prompt: {
-    "config:package:description": "Some dummy description"
-  }
-});
--- test/config:package:keywords.test.ts
+++
@@ -1,9 +0,0 @@
-import testGenerator from "./common/test-generator";
-
-testGenerator({
-  title: "Setting package keywords",
-  command: "yo wupjs:config:package:keywords",
-  prompt: {
-    "config:package:keywords": "foo, bar, quux"
-  }
-});
