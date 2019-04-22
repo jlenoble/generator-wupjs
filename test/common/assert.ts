ceab5130193eaf05089eff1f946e0c57adedffec
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-22 16:12:00 +0200

    Clearer error message

--- test/common/assert.ts
+++ test/common/assert.ts
@@ -1,6 +1,14 @@
 import { expect } from "chai";
 
 export function assertSameFileNames(files1: string[], files2: string[]): void {
-  expect(files1).to.have.length(files2.length);
-  expect([...new Set([...files1, ...files2])]).to.have.length(files2.length);
+  try {
+    expect(files1).to.have.length(files2.length);
+    expect([...new Set([...files1, ...files2])]).to.have.length(files2.length);
+  } catch (e) {
+    throw new Error(
+      `Files in scratchDir
+${JSON.stringify(files1.sort(), undefined, 2)}, but expected
+${JSON.stringify(files2.sort(), undefined, 2)}`
+    );
+  }
 }
