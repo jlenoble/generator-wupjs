a1757a1614872c5ae82e094cf5947eb2b36e7bda
Jason Lenoble <jason.lenoble@gmail.com>
2019-05-12 11:27:27 +0200

    Externalized default prompt

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,19 +1,10 @@
 import testGenerator from "./common/test-generator";
-import { configFiles, gulpFiles } from "./common/default-options";
+import { configFiles, gulpFiles, prompt } from "./common/default-options";
 
 testGenerator({
   title: "Testing main generator: Vanilla",
   command: "yo wupjs",
-  prompt: {
-    "config:author:name": "Me Me",
-    "config:author:email": "me@example.com",
-    "config:license": ["MIT"],
-    "config:package:name": "awesome-app",
-    "config:package:version": "1.2.3",
-    "config:package:description": "Some dummy description",
-    "config:package:keywords": "foo, bar, quux",
-    "config:github:username": "me-me"
-  },
+  prompt,
   assertContent: {
     ...configFiles,
     ...gulpFiles,
@@ -26,14 +17,7 @@ testGenerator({
   title: "Testing main generator: with Typescript",
   command: "yo wupjs",
   prompt: {
-    "config:author:name": "Me Me",
-    "config:author:email": "me@example.com",
-    "config:license": ["MIT"],
-    "config:package:name": "awesome-app",
-    "config:package:version": "1.2.3",
-    "config:package:description": "Some dummy description",
-    "config:package:keywords": "foo, bar, quux",
-    "config:github:username": "me-me",
+    ...prompt,
     "config:languages:typescript": true
   },
   assertContent: {
@@ -49,14 +33,7 @@ testGenerator({
   title: "Testing main generator: with Jupyter",
   command: "yo wupjs",
   prompt: {
-    "config:author:name": "Me Me",
-    "config:author:email": "me@example.com",
-    "config:license": ["MIT"],
-    "config:package:name": "awesome-app",
-    "config:package:version": "1.2.3",
-    "config:package:description": "Some dummy description",
-    "config:package:keywords": "foo, bar, quux",
-    "config:github:username": "me-me",
+    ...prompt,
     "config:languages:jupyter": true
   },
   assertContent: {
@@ -72,14 +49,7 @@ testGenerator({
   title: "Testing main generator: with ANTLR4",
   command: "yo wupjs",
   prompt: {
-    "config:author:name": "Me Me",
-    "config:author:email": "me@example.com",
-    "config:license": ["MIT"],
-    "config:package:name": "awesome-app",
-    "config:package:version": "1.2.3",
-    "config:package:description": "Some dummy description",
-    "config:package:keywords": "foo, bar, quux",
-    "config:github:username": "me-me",
+    ...prompt,
     "config:languages:antlr4": true
   },
   assertContent: {
--- test/common/default-options.ts
+++ test/common/default-options.ts
@@ -1,10 +1,12 @@
-export const configFiles: Wup.Options = {
+type Options = Wup.Options;
+
+export const configFiles: Options = {
   ".babelrc": true,
   ".eslintrc": true,
   ".gitignore": true
 };
 
-export const gulpFiles: Wup.Options = {
+export const gulpFiles: Options = {
   "gulpfile.babel.js": true,
   "gulp/build.js": true,
   "gulp/clean.js": true,
@@ -14,6 +16,17 @@ export const gulpFiles: Wup.Options = {
   "gulp/lint.js": true,
   "gulp/dist-build.js": true,
   "gulp/dist-clean.js": true
+};
+
+export const prompt: Options = {
+  "config:author:name": "Me Me",
+  "config:author:email": "me@example.com",
+  "config:license": ["MIT"],
+  "config:package:name": "awesome-app",
+  "config:package:version": "1.2.3",
+  "config:package:description": "Some dummy description",
+  "config:package:keywords": "foo, bar, quux",
+  "config:github:username": "me-me"
 };
 
 export default { configFiles, gulpFiles };
