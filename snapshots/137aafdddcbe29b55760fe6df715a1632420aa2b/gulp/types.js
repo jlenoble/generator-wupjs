6a4dc19171320557caea9c891ec02a3609eed113
Jason Lenoble <jason.lenoble@gmail.com>
2019-05-25 18:41:34 +0200

    Snapshots after adding types to prepublish

--- snapshots/137aafdddcbe29b55760fe6df715a1632420aa2b/gulp/types.js
+++ snapshots/137aafdddcbe29b55760fe6df715a1632420aa2b/gulp/types.js
@@ -1,31 +1,20 @@
 import gulp from "gulp";
 import ts from "gulp-typescript";
 
-const buildDir = "build";
-const buildSrcDir = "build/src";
+const srcDir = "src";
 const libDir = "lib";
 const libGlob = ["src/**/*.js", "src/**/*.ts"];
-const declareGlob = ["build/src/**/*.d.ts"];
 const tsProject = ts.createProject("tsconfig.json");
 
 const handleTypes = () => {
   const tsResult = gulp
     .src(libGlob, {
-      base: process.cwd(),
+      base: srcDir,
       since: gulp.lastRun(handleTypes)
     })
     .pipe(tsProject());
 
-  return tsResult.dts.pipe(gulp.dest(buildDir));
+  return tsResult.dts.pipe(gulp.dest(libDir));
 };
 
-const copyTypes = () => {
-  return gulp
-    .src(declareGlob, {
-      base: buildSrcDir,
-      since: gulp.lastRun(copyTypes)
-    })
-    .pipe(gulp.dest(libDir));
-};
-
-gulp.task("types", gulp.series(handleTypes, copyTypes));
+gulp.task("types", handleTypes);
--- snapshots/27bb1314f40a52d94735907927a75d8541ff173d/gulp/prepublish.js
+++ snapshots/27bb1314f40a52d94735907927a75d8541ff173d/gulp/prepublish.js
@@ -5,8 +5,14 @@ import "./lint";
 import "./dist-clean";
 import "./doc";
 import "./dist-test";
+import "./types";
 
 gulp.task(
   "prepublish",
-  gulp.series("test", gulp.parallel("lint", "dist-clean", "doc"), "dist-test")
+  gulp.series(
+    "test",
+    gulp.parallel("lint", "dist-clean", "doc"),
+    "dist-test",
+    "types"
+  )
 );
