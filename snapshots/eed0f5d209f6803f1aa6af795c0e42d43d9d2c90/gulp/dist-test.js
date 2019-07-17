8d1996c850c7f9cfbc3b13df6d7753667c7053d7
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-17 18:35:08 +0200

    Snapshots after fixing dist-test gulp task

--- snapshots/eed0f5d209f6803f1aa6af795c0e42d43d9d2c90/gulp/dist-test.js
+++ snapshots/eed0f5d209f6803f1aa6af795c0e42d43d9d2c90/gulp/dist-test.js
@@ -1,15 +1,26 @@
 import { src, task, series } from "gulp";
 import mocha from "gulp-mocha";
+import { resolveGlob } from "polypath";
+import streamToPromise from "stream-to-promise";
 
 import "./dist-build";
 
 const testGlob = ["build/docs/examples/**/*.test.js"];
 
-export const handleDistTest = () => {
-  return src(testGlob, { read: false }).pipe(
-    mocha({
-      reporter: "mochawesome"
-    })
+export const handleDistTest = async () => {
+  const files = await resolveGlob(testGlob);
+
+  if (!files.length) {
+    // Prevent Mocha from looking around if no example test file are found
+    return;
+  }
+
+  await streamToPromise(
+    src(testGlob, { read: false }).pipe(
+      mocha({
+        reporter: "mochawesome"
+      })
+    )
   );
 };
 
