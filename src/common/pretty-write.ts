dc07334af013b8030accfc8af1a2c889f8fb472d
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-23 12:57:41 +0200

    Use gen typescript parser if not found locally
    Prevents an exception when initiating a new typescript package

--- src/common/pretty-write.ts
+++ src/common/pretty-write.ts
@@ -1,6 +1,8 @@
 import BaseGenerator from "./base-generator";
 import ejs from "ejs";
 import { CLIEngine } from "eslint";
+import path from "path";
+import fse from "fs-extra";
 
 type Options = CLIEngine.Options;
 
@@ -17,10 +19,33 @@ export default function prettyWrite(
   text = ejs.compile(text)(props);
 
   if (!engine) {
-    engine = new CLIEngine({
-      ...(gen.getProp("config:eslint") as Options),
-      fix: true
-    });
+    if (gen.getProp("config:languages:typescript")) {
+      try {
+        fse.statSync(
+          path.join(process.cwd(), "node_modules", "@typescript-eslint/parser")
+        );
+
+        engine = new CLIEngine({
+          ...(gen.getProp("config:eslint") as Options),
+          fix: true
+        });
+      } catch (e) {
+        engine = new CLIEngine({
+          ...(gen.getProp("config:eslint") as Options),
+          parser: path.join(
+            __dirname,
+            "../../node_modules",
+            "@typescript-eslint/parser"
+          ),
+          fix: true
+        });
+      }
+    } else {
+      engine = new CLIEngine({
+        ...(gen.getProp("config:eslint") as Options),
+        fix: true
+      });
+    }
   }
 
   const {
