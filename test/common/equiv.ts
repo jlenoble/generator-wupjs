8fefcf5ef3354bc5fbc5c62697bac7ad2aebc462
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-31 16:56:39 +0200

    Handle preversions

--- test/common/equiv.ts
+++ test/common/equiv.ts
@@ -1,7 +1,7 @@
 const equiv = (text: string): string => {
   let txt = text;
 
-  txt = txt.replace(/"\^\d+\.\d+\.\d+"/g, '"^1.2.3"');
+  txt = txt.replace(/"\^\d+\.\d+\.\d+(?:-\d+)?"/g, '"^1.2.3"');
 
   return txt;
 };
