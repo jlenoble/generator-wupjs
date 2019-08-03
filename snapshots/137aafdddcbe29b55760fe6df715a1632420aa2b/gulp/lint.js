610ff286da643db7b7df62cda419961cbd5b1a4d
Jason Lenoble <jason.lenoble@gmail.com>
2019-08-03 13:05:55 +0200

    Snapshots after adding eslint report

--- snapshots/137aafdddcbe29b55760fe6df715a1632420aa2b/gulp/lint.js
+++ snapshots/137aafdddcbe29b55760fe6df715a1632420aa2b/gulp/lint.js
@@ -1,13 +1,43 @@
-import gulp from "gulp";
+import gulp, { series } from "gulp";
+import fs from "fs";
+import path from "path";
+import mkdirp from "mkdirp";
 import eslint from "gulp-eslint";
 
+const reportDir = "eslint-report";
 const srcGlob = ["src/**/*.js", "test/**/*.js", "src/**/*.ts", "test/**/*.ts"];
+
+const createReportDir = path => {
+  const mkReportDirp = () =>
+    new Promise((resolve, reject) => {
+      mkdirp(path, err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+
+  return mkReportDirp;
+};
 
 export const handleLint = () => {
   return gulp
     .src(srcGlob)
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(eslint.format())
+    .pipe(
+      eslint.format(
+        "html",
+        fs.createWriteStream(path.join(reportDir, "report.html"))
+      )
+    )
+    .pipe(
+      eslint.format(
+        "json-with-metadata",
+        fs.createWriteStream(path.join(reportDir, "report.json"))
+      )
+    );
 };
 
-gulp.task("lint", handleLint);
+gulp.task("lint", series(createReportDir(reportDir), handleLint));
