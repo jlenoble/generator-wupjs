0c85e109be0380576d46e01314c737d35701e3df
Jason Lenoble <jason.lenoble@gmail.com>
2020-12-02 19:17:59 +0100

    added todo task

--- src/config:gulp/index.ts
+++ src/config:gulp/index.ts
@@ -154,6 +154,7 @@ export default class Gulp extends Base {
       "doc",
       "prepublish",
       "push",
+      "todo",
     ];
 
     const buildDir = this.getProp("config:paths:build") as Path;
